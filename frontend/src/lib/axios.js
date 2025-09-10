aeb10b8204645e544197fec5863671d435ee1491
Mo'men ElSheemy <91328311+ElSheemy11@users.noreply.github.com>
2025-09-10 20:16:41 +0300

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,6 +4,6 @@ export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "/api"
-      : "https://ed1fc1d275bf.ngrok-free.app/api"
+      : "https://ed1fc1d275bf.ngrok-free.app/api",
   withCredentials: true,
 });
